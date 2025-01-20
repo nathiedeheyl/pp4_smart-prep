5ceab96373a65a49fddf3516661fde359a9f5f0f
Nathalie von <nath.vonheyl@gmail.com>
2025-01-20 12:04:15 +0000

    chore config: set SECRET_KEY environ var

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i1+sa5a%jvh6h2z$#ah7!4=syj*_n_xs!9li1ux_1e_$=2qj)^'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
