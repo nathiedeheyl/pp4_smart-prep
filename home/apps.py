08eb88505ce63ee4d4364fe6a26a92a989dd6067
Nathalie von <nath.vonheyl@gmail.com>
2025-01-27 18:19:06 +0000

    feat: create HomePageContent model for home page

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ShoppingListConfig(AppConfig):
+class HomePageContent(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
--- smartPrep/urls.py
+++ smartPrep/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from authentication import views as index_views
+from home import views as index_views
 
 urlpatterns = [
     path('', index_views.index, name='index'),
