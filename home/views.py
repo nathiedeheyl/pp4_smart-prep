9a43b63cdc13d1b13cbf4c76ade301b2f9001e16
Nathalie von <nath.vonheyl@gmail.com>
2025-01-28 08:00:57 +0000

    feat: add home_content view to render home page request

--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import HomePageContent
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world!")
+
+def render_home(request):
+    """
+    Renders the home page
+    """
+    home_content = HomePageContent.objects.all().order_by('-updated_at').first()
+
+    return render(
+        request,
+        "home/home.html",
+        {"home_content": home_content},
+    )
