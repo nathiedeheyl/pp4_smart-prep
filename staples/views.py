191be45fbdf9f782e1aefe7653eb1496eff3b076
Nathalie von <nath.vonheyl@gmail.com>
2025-01-29 17:32:21 +0000

    feat: add staples list view for authenticated user

--- staples/views.py
+++ staples/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import StapleItem
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def render_staples_list(request):
+    """
+    Renders the user's personal staples list
+    """
+    staples = StapleItem.objects.filter(user=request.user)
+    return render(
+        request, 
+        "staples/staples.html", 
+        {"staples": staples}
+    )
