d2785bef3f1c906bd0904c367b6204eef78cb410
Nathalie von <nath.vonheyl@gmail.com>
2025-01-29 20:57:03 +0000

    fix: resolve NameError undefined 'edit_staple' URL reference and staple.id parameter

--- staples/urls.py
+++ staples/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('staples', views.render_staples_list, name='staples'),
+    path('staples/edit/<int:staple_id>/', views.edit_staple, name='edit_staple'),
 ]
--- staples/views.py
+++ staples/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
 from .models import StapleItem
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from .forms import StapleHandling
 
 @login_required
 def render_staples_list(request):
     """
-    Renders the user's personal staples list
+    Renders the user's personal staples list and handles form submissions
     """
     staples = StapleItem.objects.filter(user=request.user)
 
@@ -27,3 +29,24 @@ def render_staples_list(request):
             "comment_form": form,
         },
     )
+
+
+@login_required
+def edit_staple(request, staple_id):
+    """
+    View to edit staples list items
+    """
+
+    staple = get_object_or_404(StapleItem, pk=staple_id, user=request.user)
+
+    if request.method == "POST":
+        form = StapleHandling(request.POST, instance=staple)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request,
+            messages.SUCCESS, 'Staple Item Updated!')
+        else:
+            messages.add_message(request,
+            messages.ERROR, 'Error updating staple item!')
+        
+    return HttpResponseRedirect(reverse('staples'))
