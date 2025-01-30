fa05fcb42b052b1c56e4ac4e8fb5301477752e2b
Nathalie von <nath.vonheyl@gmail.com>
2025-01-30 11:13:16 +0000

    config: housekeeping collectstaticfiles

---
+++ staticfiles/js/staples.js
@@ -0,0 +1,53 @@
+const editButtons = document.getElementsByClassName("edit-btn");
+const stapleForm = document.getElementById("comment_form");
+const submitButton = document.getElementById("submitButton");
+
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("delete-btn");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+for (let button of editButtons) {
+    button.addEventListener("click", (e) => {
+        let stapleId = e.target.getAttribute("staple_id");
+
+        // Get list item details
+        let stapleContent = document.getElementById(`staple${stapleId}`).innerText;
+
+        // Match name from form
+        let match = stapleContent.match(/^(.*)\s(\d+(\.\d+)?)\s(\w+)$/);
+
+        // Extract item details
+        if (match) {
+            let name = match[1];
+            let quantity = match[2];
+            let measurementUnit = match[4];
+        
+            // Populate form fields
+            document.getElementById("id_name").value = name;
+            document.getElementById("id_quantity").value = quantity;
+            document.getElementById("id_measurement_unit").value = measurementUnit;
+
+            submitButton.innerText = "Update Item";
+        } else {
+            console.error("Could not parse staple content:", stapleContent);
+        }
+    });
+}
+
+/**
+* Initializes deletion functionality for the provided delete buttons.
+* 
+* For each button in the `deleteButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Updates the `deleteConfirm` link's href to point to the 
+* deletion endpoint for the specific comment.
+* - Displays a confirmation modal (`deleteModal`) to prompt 
+* the user for confirmation before deletion.
+*/
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+      let stapleId = e.target.getAttribute("staple_id");
+      deleteConfirm.href = `/staples/delete_staple/${stapleId}`;
+      deleteModal.show();
+    });
+  }
